fce85cd36fb0a36aadda2fbe2d805bc7c194ed07
fungi <sylvienuijtemans@gmail.com>
2023-09-18 14:07:17 +0200

    bestel knop

--- filmpagina.php
+++ filmpagina.php
@@ -119,6 +119,7 @@
                     </div>
                 </div>
             </div>
+            <div id="filmpagina-bestel-button"><a href="bestel.php?id=<?php echo $_GET["id"] ?>"><span>KOOP JE TICKETS</span></a></div>
         </div>
     </div>
 
